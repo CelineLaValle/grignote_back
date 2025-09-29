4a528aed8d4f6cf539d6acc8ef79cd1cf51968f6
Céline <lavalleceline@yahoo.com>
2025-09-29 23:57:02 +0200

    test

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.use((req, res) => {
 });
 
 // Lancement du serveur
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
